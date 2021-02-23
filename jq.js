f8b81c788d0ec17fcca570fc55e6b164a890a666
Miriam Dong <miriamdong@gmail.com>
2021-02-22 16:49:10 -0800

    add click event

--- jq.js
+++ jq.js
@@ -32,6 +32,30 @@ $( function () {
   } );
 
 
+  $( '.bar' ).hover( showinfo );
+  $( '.bar' ).on( 'click', whenClick );
+  $( '.bar' ).on( 'mouseleave', whenLeave );
 
 
+  function showinfo() {
+    $( this ).toggleClass( 'a' );
+  }
+
+  function whenClick() {
+    $( this ).append( '<span class="count"></span>' );
+    $( this ).css( 'width', $( this ).attr( 'data-percent' ) );
+
+    $( '.count' ).each( function () {
+      $( this ).prop( 'Counter', 0 ).animate( {
+        Counter: $( this ).parent( '.bar' ).attr( 'data-percent' )
+      }, {
+        duration: 1000,
+        easing: 'swing',
+        step: function ( now ) {
+          $( this ).text( Math.ceil( now ) + '%' );
+        }
+      } );
+    } );
+
+  }
 } );
